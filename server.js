59489a449b577d9153200fef0d1de30ec07fd560
yugalgautam33 <yugal.gautam2014@vit.ac.in>
2016-11-25 16:22:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/animate.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'animate.css'));
+});
+
 
 app.get('/ui/component.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'component.css'));
